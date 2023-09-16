9f3b70f9b7b10e3bf9292c868c733b6f34938f21
Mike Borthwick <mhborthwick@gmail.com>
2023-09-16 00:38:18 -0700

    feat: specify size

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -3,10 +3,31 @@ import loremIntl from "../index";
 
 describe("loremIntl", () => {
   describe("method: latin", () => {
-    test("returns lorem ipsum text", () => {
-      const loremIpsum = loremIntl.latin();
+    test("returns short lorem ipsum text", () => {
+      const loremIpsum = loremIntl.latin("short");
+      expect(loremIpsum).toBe(
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+      );
+    });
+
+    test("returns medium-sized lorem ipsum text", () => {
+      const loremIpsum = loremIntl.latin("medium");
+      expect(loremIpsum).toBe(
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed luctus vestibulum mi, vitae faucibus libero vehicula eu. Fusce in aliquet risus. Suspendisse et venenatis ante, ac interdum magna. Nunc vitae ipsum lacinia, dictum nisl id, posuere turpis."
+      );
+    });
+
+    test("returns long lorem ipsum text", () => {
+      const loremIpsum = loremIntl.latin("long");
       expect(loremIpsum).toBe(
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed luctus vestibulum mi, vitae faucibus libero vehicula eu. Fusce in aliquet risus. Suspendisse et venenatis ante, ac interdum magna. Nunc vitae ipsum lacinia, dictum nisl id, posuere turpis. Donec molestie massa ante, nec tristique lectus venenatis suscipit. Suspendisse at nulla bibendum, venenatis mi sit amet, dictum dui. Vivamus ante neque, sollicitudin gravida risus eu, pellentesque volutpat tortor."
+      );
+    });
+
+    test("returns medium lorem ipsum text if you do not specify size", () => {
+      const loremIpsum = loremIntl.latin();
+      expect(loremIpsum).toBe(
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed luctus vestibulum mi, vitae faucibus libero vehicula eu. Fusce in aliquet risus. Suspendisse et venenatis ante, ac interdum magna. Nunc vitae ipsum lacinia, dictum nisl id, posuere turpis."
       );
     });
   });
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 const loremIntl = {
-  latin: () => {
-    return "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed luctus vestibulum mi, vitae faucibus libero vehicula eu. Fusce in aliquet risus. Suspendisse et venenatis ante, ac interdum magna. Nunc vitae ipsum lacinia, dictum nisl id, posuere turpis. Donec molestie massa ante, nec tristique lectus venenatis suscipit. Suspendisse at nulla bibendum, venenatis mi sit amet, dictum dui. Vivamus ante neque, sollicitudin gravida risus eu, pellentesque volutpat tortor.";
+  latin: (size?: "short" | "medium" | "long") => {
+    const map = {
+      short: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      medium:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed luctus vestibulum mi, vitae faucibus libero vehicula eu. Fusce in aliquet risus. Suspendisse et venenatis ante, ac interdum magna. Nunc vitae ipsum lacinia, dictum nisl id, posuere turpis.",
+      long: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed luctus vestibulum mi, vitae faucibus libero vehicula eu. Fusce in aliquet risus. Suspendisse et venenatis ante, ac interdum magna. Nunc vitae ipsum lacinia, dictum nisl id, posuere turpis. Donec molestie massa ante, nec tristique lectus venenatis suscipit. Suspendisse at nulla bibendum, venenatis mi sit amet, dictum dui. Vivamus ante neque, sollicitudin gravida risus eu, pellentesque volutpat tortor.",
+    };
+    return map[size || "medium"];
   },
 };
 
