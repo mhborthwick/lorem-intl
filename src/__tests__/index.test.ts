4ef46c7e13bc9654fbfbab9e18d20be32da5b86f
Mike Borthwick <mhborthwick@gmail.com>
2023-09-16 01:26:15 -0700

    feat: specify language

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -31,4 +31,47 @@ describe("loremIntl", () => {
       );
     });
   });
+
+  describe("method: create", () => {
+    describe("latin", () => {
+      test("returns new instance with default text set to latin and default size set to short", () => {
+        const instance = loremIntl.create({
+          defaultLanguage: "latin",
+          defaultSize: "short",
+        });
+        expect(instance.text()).toBe(
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+        );
+      });
+
+      test("returns new instance with default text set to latin and default size set to medium", () => {
+        const instance = loremIntl.create({
+          defaultLanguage: "latin",
+          defaultSize: "medium",
+        });
+        expect(instance.text()).toBe(
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed luctus vestibulum mi, vitae faucibus libero vehicula eu. Fusce in aliquet risus. Suspendisse et venenatis ante, ac interdum magna. Nunc vitae ipsum lacinia, dictum nisl id, posuere turpis."
+        );
+      });
+
+      test("returns new instance with default text set to latin and default size set to long", () => {
+        const instance = loremIntl.create({
+          defaultLanguage: "latin",
+          defaultSize: "long",
+        });
+        expect(instance.text()).toBe(
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed luctus vestibulum mi, vitae faucibus libero vehicula eu. Fusce in aliquet risus. Suspendisse et venenatis ante, ac interdum magna. Nunc vitae ipsum lacinia, dictum nisl id, posuere turpis. Donec molestie massa ante, nec tristique lectus venenatis suscipit. Suspendisse at nulla bibendum, venenatis mi sit amet, dictum dui. Vivamus ante neque, sollicitudin gravida risus eu, pellentesque volutpat tortor."
+        );
+      });
+    });
+  });
+
+  describe("method: text", () => {
+    test("returns medium-sized lorem ipsum by default", () => {
+      const instance = loremIntl.text();
+      expect(instance).toBe(
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed luctus vestibulum mi, vitae faucibus libero vehicula eu. Fusce in aliquet risus. Suspendisse et venenatis ante, ac interdum magna. Nunc vitae ipsum lacinia, dictum nisl id, posuere turpis."
+      );
+    });
+  });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,31 @@
-const loremIntl = {
-  latin: (size?: "short" | "medium" | "long") => {
+interface Config {
+  defaultSize: "short" | "medium" | "long";
+  defaultLanguage: "latin";
+}
+
+class LoremIntl {
+  config: Config;
+  constructor(config?: Config) {
+    this.config = config || { defaultSize: "medium", defaultLanguage: "latin" };
+  }
+
+  latin(size?: Config["defaultSize"]) {
     const map = {
       short: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
       medium:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed luctus vestibulum mi, vitae faucibus libero vehicula eu. Fusce in aliquet risus. Suspendisse et venenatis ante, ac interdum magna. Nunc vitae ipsum lacinia, dictum nisl id, posuere turpis.",
       long: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed luctus vestibulum mi, vitae faucibus libero vehicula eu. Fusce in aliquet risus. Suspendisse et venenatis ante, ac interdum magna. Nunc vitae ipsum lacinia, dictum nisl id, posuere turpis. Donec molestie massa ante, nec tristique lectus venenatis suscipit. Suspendisse at nulla bibendum, venenatis mi sit amet, dictum dui. Vivamus ante neque, sollicitudin gravida risus eu, pellentesque volutpat tortor.",
     };
-    return map[size || "medium"];
-  },
-};
+    return map[size || this.config.defaultSize];
+  }
 
-export default loremIntl;
+  text() {
+    return this[this.config.defaultLanguage](this.config.defaultSize);
+  }
+
+  create(config?: Config) {
+    return new LoremIntl(config);
+  }
+}
+
+export default new LoremIntl();
