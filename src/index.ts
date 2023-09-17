f6c9cb60ae6b74e2a6d3252e360ea64a06b2289f
Mike Borthwick <mhborthwick@gmail.com>
2023-09-16 22:05:29 -0700

    refactor: add jsdocs

--- src/index.ts
+++ src/index.ts
@@ -26,40 +26,85 @@ class LoremIntl {
     };
   }
 
+  /**
+   *
+   * @param size
+   * @returns arabic text
+   */
   arabic(size?: Size) {
     return this.getText("arabic", size);
   }
 
+  /**
+   *
+   * @param size
+   * @returns hindi text
+   */
   hindi(size?: Size) {
     return this.getText("hindi", size);
   }
 
+  /**
+   *
+   * @param size
+   * @returns english text
+   */
   english(size?: Size) {
     return this.getText("english", size);
   }
 
+  /**
+   *
+   * @param size
+   * @returns spanish text
+   */
   spanish(size?: Size) {
     return this.getText("spanish", size);
   }
 
+  /**
+   *
+   * @param size
+   * @returns mandarin chinese text
+   */
   mandarin(size?: Size) {
     return this.getText("mandarin", size);
   }
 
+  /**
+   *
+   * @param size
+   * @returns latin (lorem ipsum) text
+   */
   latin(size?: Size) {
     return this.getText("latin", size);
   }
 
+  /**
+   *
+   * @param language
+   * @param size
+   * @returns placeholder text by language and size
+   */
   getText(language?: Language, size?: Size) {
     if (!language) language = this.config.defaultLanguage;
     if (!size) size = this.config.defaultSize;
     return this.languagesMap[language][size];
   }
 
+  /**
+   *
+   * @returns default placeholder text
+   */
   text() {
     return this.getText();
   }
 
+  /**
+   *
+   * @param config
+   * @returns new instance of LoremIntl
+   */
   create(config?: Config) {
     return new LoremIntl(config);
   }
